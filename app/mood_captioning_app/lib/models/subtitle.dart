d13c2ebd05f16839b0185b0f96de74945334ae88
jsangsrichan <joy.sangsrichan@gmail.com>
2021-10-24 01:38:58 -0400

    Add subtitle setup

--- app/mood_captioning_app/lib/models/subtitle.dart
+++ app/mood_captioning_app/lib/models/subtitle.dart
@@ -1,3 +1,14 @@
+import 'dart:convert';
+
+// void main() {
+//   Subtitles subtitles = Subtitles.fromJson(jsonDecode(sample));
+//   print(subtitles);
+// }
+
+Subtitles getSubtitles() {
+  return Subtitles.fromJson(jsonDecode(sample));
+}
+
 class Subtitles {
   final List<Subtitle> subtitles;
 
@@ -15,19 +26,66 @@ class Subtitle {
   String text;
   String timestampStart;
   String timestampEnd;
-  int moodCode;
+  String mood;
 
   Subtitle(
       {required this.text,
       required this.timestampStart,
       required this.timestampEnd,
-      required this.moodCode});
+      required this.mood});
 
   factory Subtitle.fromJson(Map<String, dynamic> json) {
     return Subtitle(
         text: json['text'],
         timestampStart: json['timestamp-start'],
         timestampEnd: json['timestamp-end'],
-        moodCode: json['mood-code']);
+        mood: json['mood-code']);
   }
 }
+
+String sample = '''
+[
+    {
+        "text": "sample text 1",
+        "timestamp-start": "00:00",
+        "timestamp-end": "00:03",
+        "mood-code": "angry"
+    },
+    {
+        "text": "sample text 2",
+        "timestamp-start": "00:04",
+        "timestamp-end": "00:08",
+        "mood-code": "neutral"
+    },
+    {
+        "text": "sample text 3",
+        "timestamp-start": "00:09",
+        "timestamp-end": "00:11",
+        "mood-code": "happy"
+    },
+    {
+        "text": "sample text 4",
+        "timestamp-start": "00:12",
+        "timestamp-end": "00:15",
+        "mood-code": "boredom"
+    },
+    {
+        "text": "sample text 5",
+        "timestamp-start": "00:16",
+        "timestamp-end": "00:20",
+        "mood-code": "calm"
+    },
+    {
+        "text": "sample text 6",
+        "timestamp-start": "00:21",
+        "timestamp-end": "00:23",
+        "mood-code": "disgust"
+    },
+    {
+        "text": "sample text here",
+        "timestamp-start": "00:24",
+        "timestamp-end": "00:27",
+        "mood-code": "sad"
+    }
+]
+''';
